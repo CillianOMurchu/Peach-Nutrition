e7217d32b2d0a59502d2fcc4c2c1e9fd002defa9
Cillian <cillian.murchu@gmail.com>
2025-11-30 14:14:51 +0100

    stack deployment successfull

---
+++ worker-src/index.ts
@@ -0,0 +1,20 @@
+// worker-src/index.ts
+import { SQSEvent, Context, SQSBatchResponse } from 'aws-lambda';
+
+export const handler = async (event: SQSEvent, context: Context): Promise<SQSBatchResponse> => {
+    console.log(`Received ${event.Records.length} messages from SQS.`);
+
+    const batchItemFailures: { itemIdentifier: string }[] = [];
+
+    for (const record of event.Records) {
+        try {
+            const orderPayload = JSON.parse(record.body);
+            console.log(`[ORDER PROCESSOR] Successfully processing Order ID: ${orderPayload.userId} with ${orderPayload.items.length} items.`);
+        } catch (error) {
+            console.error('Error processing SQS record:', error);
+            batchItemFailures.push({ itemIdentifier: record.messageId });
+        }
+    }
+
+    return { batchItemFailures };
+};
